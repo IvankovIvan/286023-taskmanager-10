c8bf1458df5144660c84db0c945b2b8637f2795e
Ivankov <ivan20481@gmail.com>
2019-12-01 16:13:58 +0300

    шаблон фильтров

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,4 +1,31 @@
-export const createFilterTemplate = () => {
+const createFilterMarkup = (filter, isChecked) => {
+  const {name, count} = filter;
+
+  return (
+    `<input
+        type="radio"
+        id="filter__${name}"
+        class="filter__input visually-hidden"
+        name="filter"
+        ${isChecked ? `checked` : ``}
+      />
+      <label for="filter__${name}" class="filter__label">
+        ${name} <span class="filter__${name}-count">${count}</span>
+      </label>`
+  );
+};
+
+export const createFilterTemplate = (filters) => {
+  const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0))
+    .join(`\n`);
+  return (
+    `<section class="main__filter filter container">
+      ${filtersMarkup}
+    </section>`
+  );
+};
+
+/* export const createFilterTemplate = () => {
   return (
     `<section class="main__filter filter container">
       <input
@@ -69,5 +96,5 @@ export const createFilterTemplate = () => {
       >
     </section>`
   );
-};
+};*/
 
