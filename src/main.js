fa41b640d081da2417d4d8e5bc09fdfef84fbe76
Ivankov <ivan20481@gmail.com>
2019-12-01 19:49:39 +0300

    change main.js

--- src/main.js
+++ src/main.js
@@ -4,29 +4,37 @@ import {createBoardTemplate} from './components/board.js';
 import {createTaskTemplate} from './components/task.js';
 import {createTaskEditTemplate} from './components/task-edit.js';
 import {createLoadMoreButtonTemplate} from './components/load-more-button.js';
+import {generateTasks} from './mock/task.js';
+import {generateFilters} from './mock/filter.js';
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 22;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const createArray = (count, element) => {
+/* const createArray = (count, element) => {
   new Array(count).fill(``)
     .forEach(() => render(element, createTaskTemplate(), `beforeend`));
-};
+};*/
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElemant = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElemant, createSiteMenuTemplate(), `beforeend`);
-render(siteMainElement, createFilterTemplate(), `beforeend`);
+
+const filter = generateFilters();
+render(siteMainElement, createFilterTemplate(filter), `beforeend`);
 render(siteMainElement, createBoardTemplate(), `beforeend`);
 
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
-render(taskListElement, createTaskEditTemplate(), `beforeend`);
+const tasks = generateTasks(TASK_COUNT);
 
-createArray(TASK_COUNT, taskListElement);
+render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
+tasks.slice(1).forEach((task) => render(taskListElement
+    , createTaskTemplate(task), `beforeend`));
+
+// createArray(TASK_COUNT, taskListElement);
 
 const boardElement = siteMainElement.querySelector(`.board`);
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
