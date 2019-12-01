fe3050d430da70fedf9b30cd371e93f71b292517
Ivankov <ivan20481@gmail.com>
2019-12-01 18:58:28 +0300

    шаблон карточки

--- src/components/task.js
+++ src/components/task.js
@@ -1,6 +1,40 @@
-export const createTaskTemplate = () => {
+import {MonthNames} from '../const.js';
+import {formatTime} from '../utils.js';
+
+const createHashtagsMarkup = (hashags) => {
+  return hashags
+    .map((hashtag) => {
+      return (
+        `<span class="card__hashtag-inner">
+          <span class="card__hashtag-name">
+            #${hashtag}
+          </span>
+        </span>`
+      );
+    })
+    .join(`\n`);
+};
+
+
+
+export const createTaskTemplate = (task) => {
+  const {description, tags, dueDate, color, repeatingDays} = task;
+
+  const isExpired = dueDate instanceof Date && dueDate < Date.now();
+  const isDateShoing = !!dueDate;
+
+  const date = isDateShoing ? `${dueDate.getDate()}
+  ${MonthNames[dueDate.getMonth()]}` : ``;
+  const time = isDateShoing ? formatTime(dueDate) : ``;
+
+  const hashtags = createHashtagsMarkup(Array.from(tags));
+  const repeatClass = Object.values(repeatingDays)
+    .some(Boolean) ? `card--repeat` : ``;
+
+  const deadlineClass = isExpired ? `card--deadline` : ``;
+
   return (
-    `<article class="card card--black">
+    `<article class="card card--${color} ${repeatClass} ${deadlineClass}">
       <div class="card__form">
         <div class="card__inner">
           <div class="card__control">
@@ -12,8 +46,7 @@ export const createTaskTemplate = () => {
             </button>
             <button
               type="button"
-              class="card__btn card__btn--favorites card__btn--disabled"
-            >
+              class="card__btn card__btn--favorites card__btn--disabled">
               favorites
             </button>
           </div>
@@ -25,7 +58,7 @@ export const createTaskTemplate = () => {
           </div>
   
           <div class="card__textarea-wrap">
-            <p class="card__text">Example default task with default color.</p>
+            <p class="card__text">${description}</p>
           </div>
   
           <div class="card__settings">
@@ -33,8 +66,8 @@ export const createTaskTemplate = () => {
               <div class="card__dates">
                 <div class="card__date-deadline">
                   <p class="card__input-deadline-wrap">
-                    <span class="card__date">23 September</span>
-                    <span class="card__time">11:15 PM</span>
+                    <span class="card__date">${date}</span>
+                    <span class="card__time">${time}</span>
                   </p>
                 </div>
               </div>
@@ -43,19 +76,7 @@ export const createTaskTemplate = () => {
                 <div class="card__hashtag-list">
                   <span class="card__hashtag-inner">
                     <span class="card__hashtag-name">
-                      #todo
-                    </span>
-                  </span>
-  
-                  <span class="card__hashtag-inner">
-                    <span class="card__hashtag-name">
-                      #personal
-                    </span>
-                  </span>
-  
-                  <span class="card__hashtag-inner">
-                    <span class="card__hashtag-name">
-                      #important
+                      ${hashtags}
                     </span>
                   </span>
                 </div>
@@ -67,3 +88,4 @@ export const createTaskTemplate = () => {
     </article>`
   );
 };
+
